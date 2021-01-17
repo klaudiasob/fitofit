fa5674c5324a8e824266f8cd705432c54661787b
Klaudia <klau.sobotka@gmail.com>
2021-01-17 21:19:06 +0100

    Create users.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'devise'
 gem 'factory_bot_rails'
 gem 'geocoder'
 gem 'groupdate'
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ActivitiesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     activities = Activity.all
     @weekly_distance = activities.this_week.sum(:distance)
